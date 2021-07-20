4fb703419aff0deb491ed0ec23c423a8b76175cc
Azeemudeen101 <84566231+Azeemudeen101@users.noreply.github.com>
2021-07-20 13:29:59 +0530

    Update ListOfEmails.java

--- Projects/Module 1/BulkEmailApp/src/bulkemailapp/ListOfEmails.java
+++ Projects/Module 1/BulkEmailApp/src/bulkemailapp/ListOfEmails.java
@@ -1,39 +1,14 @@
-package bulkemailapp;
+package bulkmailapp;
 
-import java.util.ArrayList;
 import java.util.HashSet;
-import java.util.TreeMap;
 
+/* CLASS : LisOfEmails
+ * METHODS : getEmails() - Store and Return Email's in HashSet
+*/
 public class ListOfEmails {
-	//Use ArrayList if we don't care about repeated email's in List
-	public ArrayList<String> emailList(){
-		ArrayList<String> emailList=new ArrayList<>();
-		emailList.add("example@gmail.com");
-		emailList.add("example@gmail.com");
-		emailList.add("example@gmail.com");
-		emailList.add("example@gmail.com");
-		return emailList;
-	}
-	
-	//Use HashSet to ensure there is no duplicate email
-	public HashSet<String> emailSet() {
+	public HashSet<String> getEmailList(){
 		HashSet<String> emailList=new HashSet<String>();
 		emailList.add("example@gmail.com");
-		emailList.add("example@gmail.com");
-		emailList.add("example@gmail.com");
-		emailList.add("example@gmail.com");
-		return emailList;
-	}
-	
-	//Use when we have Name and Email information to specify.
-	public TreeMap<String,String> emailsMap(){
-		TreeMap<String,String> emailList=new TreeMap<>();
-		//Add Email List as TreeMap Collection
-		emailList.put("Name", "youremail@gmail.com");
-		emailList.put("Name", "youremail@gmail.com");
-		emailList.put("Name", "youremail@gmail.com");
-		emailList.put("Name", "youremail@gmail.com");
-		emailList.put("Name", "youremail@gmail.com");
 		return emailList;
 	}
 }
